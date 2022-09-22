9b6bc031788a1f8e73afcb3b93531d1bb1ee8d8f
marianelamunozgutierrez@gmail.com <marianelamunozgutierrez@gmail.com>
2022-09-22 17:26:31 -0400

    Add first seccion index.html and style.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function(){
+gulp.task('styles', function() {
     gulp.src('css/styles.css')
         .pipe(autoprefixer())
         .pipe(gulp.dest('build'))
 });
 
-gulp.task('watch',function(){
-    gulp.watch('css/styles.css',['styles']);
+gulp.task('watch',function() {
+    gulp.watch('css/styles.css', ['styles'])
 });
